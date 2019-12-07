48346369fbeb5024b6e60affffe316526e759aec
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-12-07 16:48:55 +0530

    9. Successfully divided the components into different files

---
+++ star-match/src/components/PlayAgain.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PlayAgain = props => (
+  <div className="game-done">
+    <div
+      className="message"
+      style={{ color: props.gameStatus === 'lost' ? 'red' : 'green'}}
+    >
+      {props.gameStatus === 'lost' ? 'GameComponent Over' : 'Nice'}
+    </div>
+    <button onClick={props.onClick}>Play Again</button>
+  </div>
+);
+export default PlayAgain;
